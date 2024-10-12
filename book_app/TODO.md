d959907c39a2274678806dfe4e4307482c8f4c6c
Voldezhur <voldezhur@gmail.com>
2024-10-12 09:59:16 +0300

    implemented temporary fix for weird error when deleting the last item in a  list

---
+++ book_app/TODO.md
@@ -0,0 +1,2 @@
+- [ ] Reformat the home and favourite page so that they all use one main widget
+- [ ] Figure out what was causing the not in range error when deleting the last item in a list
--- book_app/lib/pages/item_page.dart
+++ book_app/lib/pages/item_page.dart
@@ -16,40 +16,49 @@ class ItemPage extends StatefulWidget {
 class _ItemPageState extends State<ItemPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(items[widget.itemIndex].title),
-        actions: [
-          IconButton(
-            onPressed: () {
-              setState(() {
-                Navigator.pop(context, true);
-              });
-            },
-            icon: const Icon(Icons.delete),
+    return widget.itemIndex < items.length
+        ? Scaffold(
+            appBar: AppBar(
+              title: Text(items[widget.itemIndex].title),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      Navigator.pop(context, true);
+                    });
+                  },
+                  icon: const Icon(Icons.delete),
+                )
+              ],
+            ),
+            body: Center(
+              child: Column(
+                children: [
+                  Image.network(
+                    items[widget.itemIndex].imageLink,
+                    height: 300,
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                        border:
+                            Border(bottom: BorderSide(color: Colors.black26))),
+                    padding: const EdgeInsets.only(top: 25),
+                    margin: const EdgeInsets.fromLTRB(25, 0, 25, 25),
+                  ),
+                  Expanded(
+                      child: SingleChildScrollView(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          child: Text(items[widget.itemIndex].description))),
+                ],
+              ),
+            ),
           )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Image.network(
-              items[widget.itemIndex].imageLink,
-              height: 300,
+        : const Center(
+            child: Text(
+              "Страница не найдена 🤷",
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.center,
             ),
-            Container(
-              decoration: const BoxDecoration(
-                  border: Border(bottom: BorderSide(color: Colors.black26))),
-              padding: const EdgeInsets.only(top: 25),
-              margin: const EdgeInsets.fromLTRB(25, 0, 25, 25),
-            ),
-            Expanded(
-                child: SingleChildScrollView(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: Text(items[widget.itemIndex].description))),
-          ],
-        ),
-      ),
-    );
+          );
   }
 }
