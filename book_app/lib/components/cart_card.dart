844d4b473988068e44d301ffaa29eedcb9d1c85f
Voldezhur <voldezhur@gmail.com>
2024-10-13 13:29:25 +0300

    Adding the rest of cart functionality and making the item cards . Created better item cards . Added the ability to increment and decrement the item in the cart . Added a slidable function to the card, allowing to quickly remove it from the cart . Added a total at the bottom of the page

--- book_app/lib/components/cart_card.dart
+++ book_app/lib/components/cart_card.dart
@@ -1,10 +1,18 @@
 import 'package:book_app/global/lists.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class CartCard extends StatefulWidget {
-  const CartCard({super.key, required this.itemIndex});
+  const CartCard(
+      {super.key,
+      required this.itemIndex,
+      required this.removeItem,
+      required this.incrementItem});
 
   final int itemIndex;
+  final Function removeItem;
+  final Function incrementItem;
 
   @override
   State<CartCard> createState() => _CartCardState();
@@ -14,18 +22,83 @@ class _CartCardState extends State<CartCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(20)),
-        color: Theme.of(context).primaryColor,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Row(
-            children: [
-              Text(cart[widget.itemIndex].item.title),
-              Text(cart[widget.itemIndex].number.toString()),
-            ],
+      margin: const EdgeInsets.all(16.0),
+      child: Slidable(
+        startActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              backgroundColor: Theme.of(context).canvasColor,
+              icon: Icons.delete,
+              label: 'Удалить',
+              onPressed: (context) => widget.removeItem(widget.itemIndex),
+            )
+          ],
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+            color: Theme.of(context).primaryColor,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.network(
+                    cart[widget.itemIndex].item.imageLink,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  Flexible(
+                    child: Text(
+                      cart[widget.itemIndex].item.title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Стоимость: ${cart[widget.itemIndex].item.price * cart[widget.itemIndex].number}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () =>
+                                widget.incrementItem(widget.itemIndex, true),
+                            icon: const Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            cart[widget.itemIndex].number.toString(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () =>
+                                widget.incrementItem(widget.itemIndex, false),
+                            icon: const Icon(
+                              Icons.remove,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- book_app/lib/pages/cart_page.dart
+++ book_app/lib/pages/cart_page.dart
@@ -10,6 +10,26 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  void _removeItem(int index) {
+    setState(() {
+      cart.removeAt(index);
+    });
+  }
+
+  void _incrementItem(int index, bool add) {
+    setState(() {
+      if (add) {
+        cart[index].number++;
+      } else {
+        cart[index].number--;
+      }
+
+      if (cart[index].number == 0) {
+        cart.removeAt(index);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,11 +44,40 @@ class _CartPageState extends State<CartPage> {
                   textAlign: TextAlign.center,
                 ),
               )
-            : ListView.builder(
-                itemCount: cart.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return CartCard(itemIndex: index);
-                },
+            : Expanded(
+                child: Stack(
+                  children: [
+                    ListView.builder(
+                      itemCount: cart.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return CartCard(
+                          itemIndex: index,
+                          removeItem: _removeItem,
+                          incrementItem: _incrementItem,
+                        );
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(20)),
+                          color: Theme.of(context).disabledColor,
+                        ),
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Суммарная стоимость корзины: ${cart.fold(0, (sum, book) => sum + (book.item.price * book.number))}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ));
   }
 }
